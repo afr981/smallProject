28571d5962cf229bb72b037cf4f7ec3d46b98451
Javier <javier.molina@buongiorno.com>
2013-12-12 10:48:32 +0100

    Cambio a referencia a fichero con typo

--- Principal.java
+++ Principal.java
@@ -1,6 +1,6 @@
 public class Principal {
     public static void main(String [] args) {
-	ReadProperites props= new ReadProperites();
+	ReadProperties props= new ReadProperites();
         System.out.println("Bienvenido a "+props.getmensaje() );
 	System.out.println("Suma "+new Suma().add(3,4));
     }
